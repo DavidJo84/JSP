e0bfd06c55c986a3f6437057de64c8fca38773fd
DavidJo84 <112602671+DavidJo84@users.noreply.github.com>
2022-11-11 16:41:36 +0900

    add file

--- Day01/src/bbs/Board.java
+++ Day01/src/bbs/Board.java
@@ -1,7 +1,7 @@
 package bbs;
 
 import java.io.IOException;
-import java.io.PrintWriter;
+//import java.io.PrintWriter;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import sun.awt.RepaintArea;
+//import sun.awt.RepaintArea;
 
 /**
  * Servlet implementation class board
