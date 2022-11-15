32191a85c1c64725dffe03b29ec1972c27fec459
DavidJo84 <112602671+DavidJo84@users.noreply.github.com>
2022-11-15 09:12:36 +0900

    add file

--- Day02/src/com/human/controller/Index.java
+++ Day02/src/com/human/controller/Index.java
@@ -9,7 +9,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
 /**
  * Servlet implementation class Index
  */
